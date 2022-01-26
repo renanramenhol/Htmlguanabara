0d0eb7858eba4ef6934de807e01c710fbc630759
renanramenhol <93455941+renanramenhol@users.noreply.github.com>
2022-01-26 20:15:46 -0300

    ex1

---
+++ Robinho/raiz quadrada/index.js
@@ -0,0 +1,45 @@
+
+
+
+
+ distanciaEntrePontos(x1, y1, x2, y2);
+
+
+function distanciaEntrePontos(x1, y1, x2, y2){
+    let d;
+    let primeiraSubtracao;
+    let primeiraPotencia;
+    let segundaSubtracao;
+    let segundaPotencia;
+    let soma;
+    let raiz;
+
+    primeiraSubtracao = x2 - x1;
+    primeiraPotencia = primeiraSubtracao ** 2;
+
+    segundaSubtracao = y2 - y1;
+    segundaPotencia = segundaSubtracao ** 2;
+
+    soma = primeiraPotencia + segundaPotencia;
+
+    raiz = Math.sqrt(soma);
+
+    d = raiz;
+
+}
+
+let valx1 = document.getElementById('valorx1').value;
+
+let valx2 = document.getElementById('valorx2').value;
+
+let valy1 = document.getElementById('valory1').value;
+
+let valy2 = document.getElementById('valory2').value;
+
+
+   
+   document.getElementById('resposta').onclick = function() {
+   distanciaEntrePontos();
+}
+
+document.getElementById("resposta").innerHTML = d;
