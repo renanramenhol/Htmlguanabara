99a583114ce1478d323ec9cdeab382676fbeb594
Renan Gomes <renanramenhol@gmail.com>
2022-05-02 13:30:05 -0300

    mi

--- alura midi/aluramidi-curso-arquivos-iniciais/main.js
+++ alura midi/aluramidi-curso-arquivos-iniciais/main.js
@@ -1,1 +1,16 @@
-document.querySelector('.tecla_pom');
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+//document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+
+
+const ListaDeTeclas = document.querySelectorAll('.tecla');
+
+    while (contador < ListaDeTeclas.length) {
+        ListaDeTeclas[contador].onclick = tocaSomPom;
+
+        contador = contador + 1;
+    }
+   
