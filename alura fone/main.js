3b672bb5a400d2c36f98575d090e666211fe4db9
Renan Gomes <renanramenhol@gmail.com>
2022-05-02 13:29:12 -0300

    alura fone

---
+++ alura fone/main.js
@@ -0,0 +1,8 @@
+//document.querySelector('input[type=tel]');
+
+var n1 = document.querySelector('.n1');
+
+function inserir (num) {
+    let numero = document.getElementById('#resultado').innerHTML;
+     document.getElementById('#resultado').innerHTML = numero + num;
+ }
